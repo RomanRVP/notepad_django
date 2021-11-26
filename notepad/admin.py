5f478e2088a94ccf4c104e3e3017288c03aaec0a
RomanRVP <spn02@ukr.net>
2021-11-26 23:03:51 +0200

    include notepad/url and admin.registration(models)

--- notepad/admin.py
+++ notepad/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Notepad, PageForNotepad
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Notepad)
+admin.site.register(PageForNotepad)
