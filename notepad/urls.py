0be82b9dffcbdad7aa0c155934e95c0a1b5475e4
RomanRVP <spn02@ukr.net>
2021-11-27 01:19:26 +0200

    add login&logout

--- notepad/urls.py
+++ notepad/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import RegistrationUser, BaseView
+
+from .views import RegistrationUser, BaseView, LoginUser, LogoutUser
 
 
 urlpatterns = [
     path('registration/', RegistrationUser.as_view(), name='registration'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', LogoutUser.as_view(), name='logout'),
     path('', BaseView.as_view(), name='homepage')
 ]
--- notepad/views.py
+++ notepad/views.py
@@ -1,5 +1,6 @@
 from django import views
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -19,6 +20,22 @@ class RegistrationUser(CreateView):
     """
     template_name = 'notepad/registration.html'
     form_class = UserCreationForm
+    success_url = reverse_lazy('login')
 
-    # ! В будущем изменить редирект на логин. !
-    success_url = reverse_lazy('homepage')
+
+class LoginUser(LoginView):
+    """
+    Авторизация пользователя.
+    """
+    template_name = 'notepad/login.html'
+    form_class = AuthenticationForm
+
+    def get_success_url(self):
+        return reverse_lazy('homepage')
+
+
+class LogoutUser(LogoutView):
+    """
+    Выход из учетной записи.
+    """
+    next_page = 'homepage'
