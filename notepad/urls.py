2268f48e670d50695db00b58f6c97b802d2b7b5b
RomanRVP <spn02@ukr.net>
2021-11-26 23:41:24 +0200

    add empty templates and registration-view

--- notepad/urls.py
+++ notepad/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import RegistrationUser, BaseView
 
 
 urlpatterns = [
-
+    path('registration/', RegistrationUser.as_view(), name='registration'),
+    path('', BaseView.as_view(), name='homepage')
 ]
--- notepad/views.py
+++ notepad/views.py
@@ -1,1 +1,24 @@
+from django import views
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class BaseView(views.View):
+    """
+    Заглушка (для домашней страницы).
+    """
+    def get(self, request, *args, **kwargs):
+        return render(request, 'notepad/base.html')
+
+
+class RegistrationUser(CreateView):
+    """
+    Регистрация пользователя.
+    """
+    template_name = 'notepad/registration.html'
+    form_class = UserCreationForm
+
+    # ! В будущем изменить редирект на логин. !
+    success_url = reverse_lazy('homepage')
